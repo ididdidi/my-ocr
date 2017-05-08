6aeb2e029a11d2226ad5df4bffb58574e0715a39
RedBeard <msg-rb@ya.ru>
2017-05-08 13:32:37 +0400

    Test001

--- myOCR/myOCR.cpp
+++ myOCR/myOCR.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
+#include <cstdlib>
 
 
 int main()
 {
+#ifdef _OPENMP
+	printf("OpenMP is supported!\n");
+#endif
+	system("pause");
     return 0;
 }
 
